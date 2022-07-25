0019cf4d43bec8bed4fada7abaa838ccf62d5f1c
Ayush Gupta <ayushgupta.ac@gmail.com>
2022-07-25 09:18:33 +0530

    422. Valid Word Square

---
+++ 422. Valid Word Square.cpp
@@ -0,0 +1,39 @@
+/*422. Valid Word Square
+Given an array of strings words, return true if it forms a valid word square.
+A sequence of strings forms a valid word square if the kth row and column read the same string, where 0 <= k < max(numRows, numColumns).
+Input: words = ["abcd","bnrt","crmy","dtye"]
+Output: true
+Explanation:
+The 1st row and 1st column both read "abcd".
+The 2nd row and 2nd column both read "bnrt".
+The 3rd row and 3rd column both read "crmy".
+The 4th row and 4th column both read "dtye".
+Therefore, it is a valid word square.
+Input: words = ["abcd","bnrt","crm","dt"]
+Output: true
+Explanation:
+The 1st row and 1st column both read "abcd".
+The 2nd row and 2nd column both read "bnrt".
+The 3rd row and 3rd column both read "crm".
+The 4th row and 4th column both read "dt".
+Therefore, it is a valid word square.
+Input: words = ["ball","area","read","lady"]
+Output: false
+Explanation:
+The 3rd row reads "read" while the 3rd column reads "lead".
+Therefore, it is NOT a valid word square.   */
+class Solution {
+public:
+    bool validWordSquare(vector<string>& a) {
+        int i, j, n=a.size();
+        for(i=0;i<n;i++) {
+            for(j=0;j<a[i].size();j++) {
+                if(j>=n || i>=a[j].size() || a[i][j] != a[j][i]) return 0;
+            }
+        }
+        return 1;
+    }
+};
+
+
+
