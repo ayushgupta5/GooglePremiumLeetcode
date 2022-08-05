a959fdb63485b85a48f16d2bfb1048369c153252
Ayush Gupta <ayushgupta.ac@gmail.com>
2022-08-05 06:48:30 +0530

    656. Coin Path

---
+++ 656. Coin Path.cpp
@@ -0,0 +1,43 @@
+/*656. Coin Path
+You are given an integer array coins (1-indexed) of length n and an integer maxJump. You can jump to any index i of the array coins if coins[i] != -1 and you have to pay coins[i] when you visit index i. In addition to that, if you are currently at index i, you can only jump to any index i + k where i + k <= n and k is a value in the range [1, maxJump].
+
+You are initially positioned at index 1 (coins[1] is not -1). You want to find the path that reaches index n with the minimum cost.
+
+Return an integer array of the indices that you will visit in order so that you can reach index n with the minimum cost. If there are multiple paths with the same cost, return the lexicographically smallest such path. If it is not possible to reach index n, return an empty array.
+
+A path p1 = [Pa1, Pa2, ..., Pax] of length x is lexicographically smaller than p2 = [Pb1, Pb2, ..., Pbx] of length y, if and only if at the first j where Paj and Pbj differ, Paj < Pbj; when no such j exists, then x < y.
+Input: coins = [1,2,4,-1,2], maxJump = 2
+Output: [1,3,5]
+Input: coins = [1,2,4,-1,2], maxJump = 1
+Output: []   */
+class Solution {
+public:
+    vector<int> cheapestJump(vector<int>& A, int B) {
+        vector<int> ans;
+        if (A.empty() || A.back() == -1) return ans;
+        int n = A.size();
+        vector<int> dp(n, INT_MAX), pos(n, -1);
+        dp[n-1] = A[n-1];
+        // working backward
+        for (int i = n-2; i >= 0; i--) {
+            if (A[i] == -1) continue;
+            for (int j = i+1; j <= min(i+B, n-1); j++) {
+                if (dp[j] == INT_MAX) continue;
+                if (A[i]+dp[j] < dp[i]) {
+                    dp[i] = A[i]+dp[j];
+                    pos[i] = j;
+                }
+            }
+        }
+        // cannot jump to An
+        if (dp[0] == INT_MAX) return ans;
+        int k = 0;
+        while (k != -1) {
+            ans.push_back(k+1);
+            k = pos[k];
+        }
+        return ans;
+    }
+};
+
+
